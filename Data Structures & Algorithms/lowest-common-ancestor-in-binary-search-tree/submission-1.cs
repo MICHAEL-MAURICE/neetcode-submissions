ed07f005205265342b7fef93203aee8f4a2fed5d
MICHAEL-MAURICE <52583744+MICHAEL-MAURICE@users.noreply.github.com>
2026-08-25 20:13:31 +0200

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.cs
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    private TreeNode res=null;
+    private int minvalue=-900;
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Dfs(root,p,q);
+        return res;
+    }
+
+    private (bool,bool) Dfs(TreeNode node, TreeNode p, TreeNode q){
+      
+      if(node==null){
+        return (false,false);
+      }
+
+      var left=  Dfs(node.left,p,q);
+      var right= Dfs(node.right,p,q);
+
+      var foundP= left.Item1 || right.Item1 ||node==p;
+      var foundQ= left.Item2 || right.Item2 || node==q; 
+
+      if(foundP && foundQ &&res == null ){
+      
+            res=node;
+        
+      }
+
+       return (foundP , foundQ);
+
+    }
+}
